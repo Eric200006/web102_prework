c82f46884095a269f3d5eeca1e78cfa7b318b71a
Vanta826 <eric.lerner06@gmail.com>
2026-01-16 15:19:50 -0500

    commit

--- index.js
+++ index.js
@@ -38,20 +38,27 @@ function addGamesToPage(games) {
 
         // add the class game-card to the list
 
+        gameCard.classList.add("game-card");
+
 
         // set the inner HTML using a template literal to display some info 
         // about each game
         // TIP: if your images are not displaying, make sure there is space
         // between the end of the src attribute and the end of the tag ("/>")
 
-
+        gameCard.innerHTML = `
+            <img src="${game.img}" alt="${game.name}" class="game-img" />
+            <h3>${game.name}</h3>
+            <p>${game.description}</p>
+        `;
         // append the game to the games-container
-
+        gamesContainer.appendChild(gameCard);
+    }
 }
 
 // call the function we just defined using the correct variable
 // later, we'll call this function using a different list of games
-
+addGamesToPage(GAMES_JSON);
 
 /*************************************************************************************
  * Challenge 4: Create the summary statistics at the top of the page displaying the
